8644b0b6326b8a23e944c1f92400c7acfbed1292
Mitch <townendmitchell123@gmail.com>
2025-03-05 00:30:26 +0000

    node progress so far, it makes sense which is good

---
+++ math.js
@@ -0,0 +1,11 @@
+const addition =  (a, b) => a + b;
+const subtract =  (a, b) => a - b;
+const multiply =  (a, b) => a * b;
+const divide =  (a, b) => a / b;
+
+module.exports = {
+    addition,
+    subtract,
+    multiply,
+    divide
+}
--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// test
+console.log('Hello!');
+
+const os = require('os');
+const path = require('path');
+const math = require('./math');
+
+console.log(os.type());
+console.log(os.version());
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
+
+console.log(math.addition(2, 3));
